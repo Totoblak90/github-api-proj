117442ac6548e7a4d91e779b72bc704f94cc8765
Totoblak90 <tobias.blaksley@hotmail.com>
2023-03-12 20:28:58 -0300

    reactivated the env variables configuration

--- server/src/prisma/prisma.service.ts
+++ server/src/prisma/prisma.service.ts
@@ -1,10 +1,11 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-    constructor() {
-        super({ datasources: { db: { url: 'postgresql://postgres:123@localhost:5434/nest?schema=public' } } })
+    constructor(private confiService: ConfigService) {
+        super({ datasources: { db: { url: confiService.get('DATABASE_URL') } } })
     }
 
 }
