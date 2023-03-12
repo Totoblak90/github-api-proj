6bcaecd77b7478e45c883f532810f93068d6228c
Totoblak90 <tobias.blaksley@hotmail.com>
2023-03-12 14:48:08 -0300

    created an input component with icon and focus effects. created a search form to filter repos by text

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -16,6 +16,9 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatSelectModule} from '@angular/material/select';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FilterComponent } from './components/filter/filter.component';
+import { InputComponent } from './ui/input/input.component';
+import { FaIconComponent } from './ui/fa-icon/fa-icon.component';
+import { InputRefDirective } from './directives/input-ref.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +27,10 @@ import { FilterComponent } from './components/filter/filter.component';
     RepoItemComponent,
     CommitListComponent,
     CommitComponent,
-    FilterComponent
+    FilterComponent,
+    InputComponent,
+    FaIconComponent,
+    InputRefDirective
   ],
   imports: [
     BrowserModule,
--- client/src/app/components/repo-list/repo-list.component.ts
+++ client/src/app/components/repo-list/repo-list.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { Repository } from 'src/app/interface/repository.interface';
 import { HttpService } from 'src/app/services/http.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'repo-list',
@@ -9,11 +11,28 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class RepoListComponent {
 
+  filterForm: FormGroup = this.fb.group({
+    search: ['']
+  })
+
   repoList: Repository[]  = [];
 
-  constructor(private httpService: HttpService) {
+  constructor(private httpService: HttpService, private fb: FormBuilder) {
+    this.fetchRepos();
+    this.subscribeToValueChanges()
+  }
+
+  private fetchRepos() {
     this.httpService.allRepos().subscribe(res => {
       this.repoList = res
     })
   }
+
+  private subscribeToValueChanges() {
+    this.filterForm.get('search')?.valueChanges.pipe(debounceTime(750)).subscribe(term => {
+      console.log(term)
+    })
+  }
+
+
 }
